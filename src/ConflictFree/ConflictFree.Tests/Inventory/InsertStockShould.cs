d2e79fec4863d1d71eb38ff866c1f970c12742c9
yeray <j.cabello.ramos@gmail.com>
2024-08-07 14:59:49 +0100

    red

--- src/ConflictFree/ConflictFree.Tests/Inventory/InsertStockShould.cs
+++ src/ConflictFree/ConflictFree.Tests/Inventory/InsertStockShould.cs
@@ -10,4 +10,16 @@ public class InsertStockShould
     service.InsertStock(productId, 10);
     Assert.Equal(10, service.GetStock(productId));
   }
+
+  [Fact(DisplayName = "register the stock that was inserted for two products")]
+  public void Test2()
+  {
+    var service = new InventoryService();
+    var product1Id = Guid.NewGuid();
+    service.InsertStock(product1Id, 10);
+    Assert.Equal(10, service.GetStock(product1Id));
+    var product2Id = Guid.NewGuid();
+    service.InsertStock(product2Id, 1);
+    Assert.Equal(1, service.GetStock(product2Id));
+  }
 }
