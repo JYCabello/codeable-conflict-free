be4b2ecba7cf64443ea2d9e431959b1c9e868d2c
yeray <j.cabello.ramos@gmail.com>
2024-08-07 14:57:46 +0100

    red

---
+++ src/ConflictFree/ConflictFree.Tests/Inventory/InsertStockShould.cs
@@ -0,0 +1,13 @@
+﻿namespace ConflictFree.Tests.Inventory;
+
+public class InsertStockShould
+{
+  [Fact(DisplayName = "register the stock that was inserted")]
+  public void Test()
+  {
+    var service = new InventoryService();
+    var productId = Guid.NewGuid();
+    service.InsertStock(productId, 10);
+    Assert.Equal(10, service.GetStock(productId));
+  }
+}
