631e68dceb3dc76e54cd7169078d43ac2793b05e
olivercallas <A18101119@idat.edu.pe>
2024-08-08 09:16:25 -0500

    red

--- src/ConflictFree/ConflictFree.Tests/InventoryRepository/InvetoryRepositoryShould.cs
+++ src/ConflictFree/ConflictFree.Tests/InventoryRepository/InvetoryRepositoryShould.cs
@@ -2,7 +2,7 @@ using ConflictFree.Tests;
 
 public class Integration
 {
-  [Fact(DisplayName = "Insertar un evento de cambio de stock y recuperar stock correcto")]
+  [Fact(DisplayName = "Insertar un evento de cambio de stock de 1 y recuperar stock correcto")]
   public async Task Test1()
   {
     var inventoryRepository = new InventoryRepository();
@@ -12,5 +12,17 @@ public class Integration
     var productStock = inventoryRepository.GetProductStock(newId);
 
     Assert.Equal(1, productStock.Stock);
+  }
+
+  [Fact(DisplayName = "Insertar un evento de cambio de stock de 2 y recuperar stock correcto")]
+  public async Task Test2()
+  {
+    var inventoryRepository = new InventoryRepository();
+    var newId = Guid.NewGuid();
+
+    inventoryRepository.InsertEvent(new StockRestored(newId, 2));
+    var productStock = inventoryRepository.GetProductStock(newId);
+
+    Assert.Equal(2, productStock.Stock);
   }
 }
